95c86f9b9a71d4d0129d84e6b192c99f7389aa12
Kolia913 <mynew1mail470@gmail.com>
2021-08-14 15:27:24 +0300

    Initial commit

---
+++ pkg/handler/list.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"github.com/Kolia913/todo-app"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (h *Handler) createList(c *gin.Context) {
+	userId, err := getUserId(c)
+
+	if err != nil {
+		return
+	}
+
+	var input todo.TodoList
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	id, err := h.services.TodoList.Create(userId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
+}
+
+func (h *Handler) getAllLists(c *gin.Context) {
+
+}
+
+func (h *Handler) getListById(c *gin.Context) {
+
+}
+
+func (h *Handler) updateList(c *gin.Context) {
+
+}
+
+func (h *Handler) deleteList(c *gin.Context) {
+
+}
---
+++ pkg/repository/repository.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"github.com/Kolia913/todo-app"
+	"github.com/jmoiron/sqlx"
+)
+
+type Authorization interface {
+	CreateUser(user *todo.User) (int, error)
+	GetUser(username, password string) (todo.User, error)
+}
+
+type TodoList interface {
+	Create(userId int, list todo.TodoList) (int, error)
+}
+
+type TodoItem interface {
+}
+
+type Repository struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{
+		Authorization: NewAuthPostgres(db),
+		TodoList: NewTodoListPostgres(db),
+	}
+}
